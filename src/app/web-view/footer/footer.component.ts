5dbbd9f86bf189b4d14b4a12826e5b03412a6e98
Nguyễn Văn Nhân <44488819+cunid29@users.noreply.github.com>
2020-12-09 09:06:53 +0700

    back-on-top

--- src/app/web-view/footer/footer.component.ts
+++ src/app/web-view/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,6 +10,19 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var btn = $('.btn-backToTop');
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.removeClass('d-none');
+      } else {
+        btn.addClass('d-none');
+      }
+    });
+
+    btn.on('click', function(e){
+      e.preventDefault();
+      $('html,body').animate({scrollTop:0},'300');
+    });
   }
 
 }
